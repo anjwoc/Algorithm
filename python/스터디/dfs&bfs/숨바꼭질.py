114f0cc873fffd493e9db3d15a90927f367aa8d2
anjwoc <anjwoc@naver.com>
2020-01-15 22:26:37 +0900

    숨바꼭질 다시 품

--- python/스터디/dfs&bfs/숨바꼭질.py
+++ python/스터디/dfs&bfs/숨바꼭질.py
@@ -1,22 +1,26 @@
 import sys
-input = sys.stdin.readline
+from collections import deque
 
-n, k = map(int, input().split())
+input = sys.stdin.readline
 
 # 걷게 되면 1초 후에 -1 또는 +1로 이동
 # 순간이동을 하면 2*X위치
 
-dx = [-1, 1, 2]
-visited = [False]*100001
-times = [-1]*100001
-times[n] = 0
-que = [n]
+def bfs():
+  que = deque()
+  que.append(n)
+  while que:
+    x = que.popleft()
+    if x == k:
+      print(times[k])
+      return
+    for cur_x in [x-1, x+1, 2*x]:
+      if 0 <= cur_x < 100001 and not times[cur_x]:
+        times[cur_x] = times[x] + 1
+        que.append(cur_x)
 
-while que:
-  x = que.pop()
-  for cur_x in [x-1, x+1, 2*x]:
-    if cur_x <= 100000 and cur_x >= 0 and not times[cur_x]==-1:
-      times[cur_x] += times[x]+1
-      que.append(cur_x)
+n, k = map(int, input().split())
+times = [0]*100001
+bfs()
 
 
---
+++ python/스터디/기초DP-1/피보나치(1003).py
@@ -0,0 +1,11 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+ 
+def fibo(n):
+
+
+t = int(input())
+for _ in range(t):
+  n = int(input())
+
