91f5135590f0f93b7d1a446af097dfc62e0e2ced
anjwoc <anjwoc@naver.com>
2020-04-16 20:45:00 +0900

    solve

--- python/Programmmers/고득점키트/dfs-bfs/단어변환.py
+++ python/Programmmers/고득점키트/dfs-bfs/단어변환.py
@@ -9,20 +9,24 @@ def solution(begin, target, words):
             tmp = []
             for word in words:
                 cnt = 0
-                for i in range(len(char)):
-                    if char[i] != word[i]:
+                for j in range(len(char)):
+                    if char[j] != word[j]:
                         cnt += 1
                     if cnt == 2:
                         break
                 if cnt == 1:
+                    print(char, word)
                     tmp.append(word)
                     words.remove(word)
         ans_cnt += 1
         if target in tmp:
+            print(target)
             return ans_cnt
         else:
             ans = tmp
+
     return 0
+
 
 begin, target, words = "hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]
 
