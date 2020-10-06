157f5d3d62b7b1c12ac6eac64c99507ccf592153
anjwoc <anjwoc@naver.com>
2020-10-06 18:01:54 +0000

    빙산 복습

--- python/라이 문제집/bj-2573-빙산.py
+++ python/라이 문제집/bj-2573-빙산.py
@@ -2,13 +2,17 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-def bfs(i, j, visited):
-    que = deque()
-    melting_que = deque()
-    visited[i][j] = 1
-    que.append((i, j))
+dx = (-1, 0, 1, 0)
+dy = (0, 1, 0, -1)
+n, m = map(int, input().split())
+arr = []
 
-    print(visited)
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+def bfs(i, j, visited):
+    que = deque([(i, j)])
+    melting_que = deque()
 
     while que:
         x, y = que.popleft()
@@ -16,49 +20,42 @@ def bfs(i, j, visited):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<= nx <= n-1 and 0<= ny <= m-1 and visited[nx][ny] == 0:
-                if arr[nx][ny] != 0:
-                    visited[nx][ny] = 1
-                    que.append((nx, ny))
-                else:
+            if 0<=nx<n and 0<=ny<m and (nx, ny) not in visited:
+                if arr[nx][ny] == 0:
                     cnt += 1
+                else:
+                    visited.add((nx, ny))
+                    que.append((nx, ny))
         if cnt:
-            melting_que.append([i, j, cnt])
-        # print(melting_que)
+            melting_que.append((x, y, cnt))
     return melting_que
-
+        
 
 year = 0
-dx = (-1, 1, 0, 0)
-dy = (0, 0, 1, -1)
-
-n, m = map(int, input().split())
-arr = []
-for _ in range(n):
-    arr.append(list(map(int, input().split())))
-
 while True:
+    visited = set()
+    melting_que = deque()
     cnt = 0
-    visited = [[0]*(m) for _ in range(n)]
+    
     for i in range(n-1):
-        for j in range(m-1):
-            if arr[i][j] != 0 and visited[i][j] == 0:
-                cnt+=1
+        for j in range(m-1):    
+            if arr[i][j] != 0 and (i, j) not in visited:
+                visited.add((i,j))
                 melt = bfs(i, j, visited)
-                while melt:
-                    nx, ny, value = melt.popleft()
-                    arr[nx][ny] = max(arr[nx][ny] - value, 0)
+                cnt += 1
+                
+                for x, y, val in melt:
+                    arr[x][y] = max(arr[x][y] - val, 0)
+
     if cnt == 0:
         year = 0
         break
-    if cnt > 2:
+                
+    if cnt >= 2:
         break
     year += 1
 
-
 print(year)
     
-            
+       
 
-
-        
