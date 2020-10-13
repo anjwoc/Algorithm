49e174762245b9b0394a3d72d95ddfcaf27a8ffb
anjwoc <anjwoc@naver.com>
2020-10-13 13:44:41 +0000

    백준 보물섬 풀이 시간 최적화 (in 연산자 제거)

--- python/문제집/BFS(하-중)/bj-2589-보물섬.py
+++ python/문제집/BFS(하-중)/bj-2589-보물섬.py
@@ -10,8 +10,6 @@ def bfs(arr, i, j):
     maps = [[0] * m for _ in range(n)]
     que = deque()
     que.append((i, j))
-    visited = set()
-    visited.add((i, j))
     maps[i][j] = 1
     ans = 0
     while que:
@@ -19,13 +17,12 @@ def bfs(arr, i, j):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and (nx, ny) not in visited:
+            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 0:
                 if arr[nx][ny] == 'L':
-                    visited.add((nx, ny))
                     que.append((nx, ny))
                     maps[nx][ny] = maps[x][y] + 1
                     ans = max(maps[nx][ny], ans)
-    return (ans, maps)
+    return ans
 
 
 n, m = map(int, input().split())
@@ -35,12 +32,8 @@ for _ in range(n):
     arr.append(list(input()))
 
 ans = 0
-result = []
 for i in range(n):
     for j in range(m):
         if arr[i][j] == 'L':
-            tmp, maps = bfs(arr, i, j)
-            if ans < tmp:
-                ans = tmp
-                result = maps
+            ans = max(bfs(arr, i, j), ans)
 print(ans-1)
