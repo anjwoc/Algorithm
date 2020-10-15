b3332b927a29175ad0fc51e60e8b6322d3715b49
anjwoc <anjwoc@naver.com>
2020-10-15 14:37:06 +0000

    백준 암호만들기

---
+++ lecture/패스트캠퍼스/lec19/bj-1759-암호만들기.py
@@ -0,0 +1,17 @@
+import sys
+from itertools import combinations
+input: lambda: sys.stdin.readline().strip()
+
+vowels = ('a', 'e', 'i', 'o', 'u')
+l, c = map(int, input().split())
+arr = input().split()
+arr.sort()
+
+for password in combinations(arr, l):
+    cnt = 0
+    for i in password:
+        if i in vowels:
+            cnt += 1
+
+    if cnt >= 1 and cnt <= l - 2:
+        print(''.join(password))
