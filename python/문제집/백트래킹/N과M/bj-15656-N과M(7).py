52eb50d103bd23d51a4da75aa2b6c723f502dc58
anjwoc <anjwoc@naver.com>
2020-10-19 05:26:49 +0000

    백준 N과M(7)

---
+++ python/문제집/백트래킹/N과M/bj-15656-N과M(7).py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+arr = sorted(map(int, input().split()))
+check = [False] * (n+1)
+ans = []
+
+
+def solve(cnt):
+    if cnt == m:
+        print(' '.join(map(str, ans)))
+    else:
+        for i in range(n):
+            ans.append(arr[i])
+            solve(cnt+1)
+            ans.pop()
+
+
+solve(0)
