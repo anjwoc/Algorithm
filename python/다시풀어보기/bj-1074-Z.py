9e9b07ae8edb31a4d39725402946ea7a8d1c8acd
anjwoc <anjwoc@naver.com>
2020-10-22 01:40:26 +0000

    백준 Z

---
+++ python/다시풀어보기/bj-1074-Z.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+n, r, c = map(int, input().split())
+ans = 0
+
+
+def solve(n, x, y):
+    global ans
+    if n == 2:
+        if x == r and y == c:
+            print(ans)
+            return
+        ans += 1
+        if x == r and y+1 == c:
+            print(ans)
+            return
+        ans += 1
+        if x+1 == r and y == c:
+            print(ans)
+            return
+        ans += 1
+        if x+1 == r and y+1 == c:
+            print(ans)
+            return
+        ans += 1
+        return
+
+    solve(n/2, x, y)
+    solve(n/2, x, y + n/2)
+    solve(n/2, x+n/2, y)
+    solve(n/2, x+n/2, y+n/2)
+
+
+solve(2**n, 0, 0)
