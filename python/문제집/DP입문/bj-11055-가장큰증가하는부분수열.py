0c744c51a68bf059cab99a83afaa4900fca3fdcf
anjwoc <anjwoc@naver.com>
2020-10-31 03:20:01 +0900

    1. 백준 가장 큰 증가하는 부분수열 2. 백준 가장 긴 감소하는 부분수열

---
+++ python/문제집/DP입문/bj-11055-가장큰증가하는부분수열.py
@@ -0,0 +1,15 @@
+import sys
+from copy import deepcopy
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = deepcopy(arr)
+
+for i in range(n):
+  for j in range(i + 1):
+    if arr[i] > arr[j]:
+      dp[i] = max(dp[i], dp[j] + arr[i])
+
+
+print(max(dp))
