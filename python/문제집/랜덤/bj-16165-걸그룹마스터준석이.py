92f3ae9e33a0c99984bf707e7ed39f2db30a10d9
anjwoc <anjwoc@naver.com>
2020-11-12 09:12:16 +0000

    백준 걸그룹마스터준석이

---
+++ python/문제집/랜덤/bj-16165-걸그룹마스터준석이.py
@@ -0,0 +1,28 @@
+import sys
+input: lambda: sys.stdin.readline().strip()
+
+n, m = map(int, input().split())
+arr = []
+
+for _ in range(n):
+    gname = input()
+    dic = dict()
+    dic[gname] = []
+    for _ in range(int(input())):
+        dic[gname].append(input())
+    arr.append(dic)
+
+for _ in range(m):
+    qname = input()
+    qcategory = int(input())
+
+    if qcategory == 0:
+        for target in arr:
+            if qname in target.keys():
+                for i in sorted(*target.values()):
+                    print(i)
+    elif qcategory == 1:
+        for target in arr:
+            key = list(target.keys())[0]
+            if qname in target[key]:
+                print(key)
