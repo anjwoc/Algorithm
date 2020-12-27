e76c64c87bb108761f98f39bf7080072389cc4cd
anjwoc <anjwoc@naver.com>
2020-12-27 01:25:26 +0000

    백준 -> 프로그래머스 프린터(오타 수정)

--- python/문제집/프로그래머스/level2/프린터.py
+++ python/문제집/프로그래머스/level2/프린터.py
@@ -1,46 +1,19 @@
-import heapq
 from collections import deque
 
 
 def solution(P, L):
     ans = 0
-    alpha = list(map(str, range(1, len(P)+1)))
 
-    targetIdx = L
-    items = zip(list(map(lambda x: -x, P)), alpha)
-    print(items)
-    que = deque([*items])
-    print(que)
-    target = que[L][1]
-    print(target)
-    maps = list(que)
-    heapq.heapify(maps)
-    print(maps)
+    que = deque([(v, i) for i, v in enumerate(P)])
 
-    cur = 1
-    cnt = 1
     while que:
-        x, char = que.popleft()
-        x2, prior = heapq.heappop(maps)
-        print(x, char, x2, prior, cnt)
-        if char == prior:
-            if char == target:
-                ans = cnt
-                break
-            else:
-                cnt += 1
-                continue
+        value, index = que.popleft()
+
+        if que and max(que)[0] > value:
+            que.append((value, index))
         else:
-            que.append((x, char))
-            heapq.heappush(maps, (x2, prior))
+            ans += 1
+            if index == L:
+                break
 
     return ans
-
-
-items = [
-    [[2, 1, 3, 2],	2],
-    [[1, 1, 9, 1, 1, 1], 0]
-]
-
-for P, L in items:
-    print(solution(P, L))
