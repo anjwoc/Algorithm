b7abc6a35ec8dcdf7a52e181f42dc511add2322e
anjwoc <anjwoc@naver.com>
2022-09-06 23:51:31 +0900

    프로그래머스 크레인인형뽑기

---
+++ programmers/크레인인형뽑기.py
@@ -0,0 +1,25 @@
+def solution(board, moves):
+    stacks = []
+    ans = 0
+
+    def push(ans, st, item):
+        if len(st) > 0 and st[-1] == item:
+            st.pop()
+            return 2
+
+        st.append(item)
+        return 0
+
+    for move in moves:
+        idx = int(move) - 1
+        for x in range(len(board[0])):
+            cur = board[x][idx]
+            if cur == 0:
+                continue
+            else:
+                num = push(ans, stacks, board[x][idx])
+                board[x][idx] = 0
+                ans += num
+                break
+
+    return ans
